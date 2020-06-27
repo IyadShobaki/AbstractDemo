3b70c23d75bcb196da10511b409f26386b1a003e
IyadShobaki <35826354+IyadShobaki@users.noreply.github.com>
2020-06-27 15:11:12 -0400

    Abstract Classes

---
+++ DemoLibrary/SqliteDataAccess.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class SqliteDataAccess : DataAccess //IDataAccess
+    {
+        public override string LoadConnectionString(string name)
+        {
+            string output = base.LoadConnectionString(name);
+
+            output += " (from SQLite)";
+
+            return output;
+        }
+
+        //public string LoadConnectionString(string name)
+        //{
+        //    Console.WriteLine("Load Connection String");
+        //    return "testConnectionString";
+        //}
+        public override void LoadData(string sql)
+        {
+            Console.WriteLine("Loading SQLite Data");
+        }
+        public override void SaveData(string sql)
+        {
+            Console.WriteLine("Saving data to SQLite");
+        }
+    }
+}
